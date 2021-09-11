ca429faeb0b0de0a5268e0de679b76039d702b10
mag1pie <67106393+mag1pie@users.noreply.github.com>
2021-09-11 18:28:05 -0400

    Update util.py

--- schpf/util.py
+++ schpf/util.py
@@ -229,3 +229,140 @@ def minibatch_ix_generator(ncells, batchsize):
             res = ixs[start:stop]
         start = stop % ncells # need mod for case where ncells=batchsize
         yield res
+
+        
+from sklearn import metrics
+
+def get_param_dfs(model):
+    eta_shp = pd.Series(np.ravel(model.eta.vi_shape), name=model.name)
+    eta_rte = pd.Series(np.ravel(model.eta.vi_rate), name=model.name)
+    beta_shp = pd.DataFrame(model.beta.vi_shape.T)
+    beta_shp.index = model.name + ':' + (beta_shp.index + 1).astype(str)
+    beta_rte = pd.DataFrame(model.beta.vi_rate.T, index=beta_shp.index)
+    return eta_shp, eta_rte, beta_shp, beta_rte
+
+
+def get_spectra(models):
+    eta_shp, eta_rte, beta_shp, beta_rte = zip(*[get_param_dfs(m) for m in models])
+    return pd.concat(eta_shp, axis=1).T, pd.concat(eta_rte,axis=1).T, pd.concat(beta_shp), pd.concat(beta_rte)
+
+
+def get_genescore_spectra(models):
+    gene_scores = []
+    for m in models:
+        gs = pd.DataFrame(m.gene_score().T)
+        gs.index = m.name + ':' + (gs.index + 1).astype(str)
+        gene_scores.append(gs)
+    return pd.concat(gene_scores)
+
+
+def get_spectra_order(factor_dists, cluster_labels):
+    spectra_order = []
+    for cl in sorted(set(cluster_labels)):
+        cl_filter = cluster_labels==cl
+        if cl_filter.sum() > 1:
+            cl_dist = squareform(factor_dists[cl_filter, :][:, cl_filter])
+            cl_dist[cl_dist < 0] = 0 #Rarely get floating point arithmetic issues
+            cl_link = linkage(cl_dist, 'average')
+            cl_leaves_order = leaves_list(cl_link)
+            spectra_order += list(np.where(cl_filter)[0][cl_leaves_order])
+        else:
+            ## Corner case where a component only has one element
+            spectra_order += list(np.where(cl_filter)[0])
+    return spectra_order
+
+
+# returns the median correlation distance between factor of the row and its n nearest neighbors
+def get_local_density(factor_dists, n_neighbors, factor_labels):
+    partitioning_order  = np.argpartition(factor_dists, n_neighbors+1)[:, :n_neighbors+1] 
+    # finds the n nearest neighbors for every factor across all N_PER*nmodels models (self correlation dist is 0)
+    # ndims(partitioning_order) = total n of factors * (n_neighbors+1)
+    distance_to_nearest_neighbors = factor_dists[np.arange(factor_dists.shape[0])[:, None], partitioning_order]
+    return pd.DataFrame(np.median(distance_to_nearest_neighbors, axis=1),
+        #distance_to_nearest_neighbors.sum(1)/(n_neighbors), 
+                        columns=['local_density'],
+                        index=factor_labels)
+
+
+def make_consensus_plot(factor_dists, cluster_labels, local_density, density_threshold, dist_cmap='Reds_r'):
+    width_ratios = [0.5, 9, 0.5, 4, 1]
+    height_ratios = [0.5, 9]
+    fig = plt.figure(figsize=(sum(width_ratios), sum(height_ratios)))
+    gs = gridspec.GridSpec(len(height_ratios), len(width_ratios), fig,
+                       0.01, 0.01, 0.98, 0.98,
+                       height_ratios=height_ratios,
+                       width_ratios=width_ratios,
+                       wspace=0, hspace=0)
+
+    dist_ax = fig.add_subplot(gs[1,1], xscale='linear', yscale='linear',
+                                      xticks=[], yticks=[],xlabel='', ylabel='',
+                                      frameon=True)
+
+    spectra_order = get_spectra_order(factor_dists, cluster_labels)
+    D = factor_dists[spectra_order, :][:, spectra_order]
+    dist_im = dist_ax.imshow(D, interpolation='none', cmap=dist_cmap, aspect='auto',
+                                rasterized=True)
+    
+    left_ax = fig.add_subplot(gs[1,0], xscale='linear', yscale='linear', xticks=[], yticks=[],
+                xlabel='', ylabel='', frameon=True)
+    left_ax.imshow(cluster_labels.values[spectra_order].reshape(-1, 1),
+                            interpolation='none', cmap='Spectral', aspect='auto',
+                            rasterized=True)
+    top_ax = fig.add_subplot(gs[0,1], xscale='linear', yscale='linear', xticks=[], yticks=[],
+                xlabel='', ylabel='', frameon=True)
+    top_ax.imshow(cluster_labels.values[spectra_order].reshape(1, -1), 
+                              interpolation='none', cmap='Spectral', aspect='auto',
+                                rasterized=True)
+    
+    hist_gs = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs[1, 3],
+                                   wspace=0, hspace=0)
+    hist_ax = fig.add_subplot(hist_gs[0,0], xscale='linear', yscale='linear',
+                xlabel='', ylabel='', frameon=True, title='Local density histogram')
+    hist_ax.hist(local_density.values, bins=np.linspace(0,1,50)#np.sqrt(2),50)
+            )
+    hist_ax.yaxis.tick_right()
+
+    xlim = hist_ax.get_xlim()
+    ylim = hist_ax.get_ylim()
+    #density_threshold = xlim[1] + 1
+    if density_threshold < xlim[1]:
+        density_filter = local_density.iloc[:, 0] < density_threshold
+        hist_ax.axvline(density_threshold, linestyle='--', color='k')
+        hist_ax.text(density_threshold  + 0.02, ylim[1] * 0.95, 'filtering\nthreshold\n\n', va='top')
+        hist_ax.set_xlim(xlim)
+        hist_ax.set_xlabel('Mean distance to k nearest neighbors\n\n%d/%d (%.0f%%) spectra above threshold\nwere removed prior to clustering'%(sum(~density_filter), len(density_filter), 100*(~density_filter).mean()))
+        
+    return fig, (dist_ax, left_ax, top_ax, hist_ax)
+
+
+def refit_local_params(X, global_params, nfactors, bp, dp, a=0.3, c=0.3, project_kw={}):
+    """
+    """
+    project_defaults = dict(verbose=True, max_iter=50, check_freq=5)
+    eta_shp, eta_rte, beta_shp, beta_rte = global_params
+    
+    # make a model
+    eta = schpf.HPF_Gamma(np.ravel(eta_shp), np.ravel(eta_rte))
+    beta = schpf.HPF_Gamma(beta_shp.T.values, beta_rte.T.values)
+    model = schpf.scHPF(nfactors, eta=eta, beta=beta, bp=bp, dp=dp, a=a, c=c)           
+    
+    # setup projection kwarg
+    for k,v in project_defaults.items():
+        if k not in project_kw.keys():
+            project_kw[k] = v
+    loss = model.project(X, replace=True, **project_kw)
+    model.loss = loss
+    
+    return model
+
+
+def get_ranked_genes(model, genes, name_col=1):
+    gene_score = model.gene_score()
+
+    # rank the genes by gene_score
+    ranks = np.argsort(gene_score, axis=0)[::-1]
+    ranked_genes = []
+    for i in range(gene_score.shape[1]):
+        ranked_genes.append(genes[ranks[:,i], name_col])
+    ranked_genes = pd.DataFrame(np.stack(ranked_genes).T)
+    return ranked_genes
