cd9f9b0d3cbab6a80a04e0a5d8c50f288587a6fa
mag1pie <67106393+mag1pie@users.noreply.github.com>
2021-11-14 18:53:38 -0500

    Update util.py

--- schpf/util.py
+++ schpf/util.py
@@ -8,11 +8,12 @@ from scipy.io import mmread
 import igraph as ig
 
 import schpf
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, coo_matrix
 from scipy.stats import hypergeom
 from scipy.spatial.distance import squareform
 #from sklearn.decomposition.nmf import non_negative_factorization
 #from sklearn.cluster import KMeans
+from sklearn import metrics
 from sklearn.metrics import silhouette_score
 from sklearn.utils import sparsefuncs
 import sklearn
@@ -25,6 +26,7 @@ import loompy
 from tqdm import tqdm
 
 import matplotlib as mpl
+from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib import pyplot as plt
 plt.rcParams['pdf.fonttype'] = 42
 plt.rcParams['ps.fonttype'] =42
@@ -32,10 +34,14 @@ import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import gridspec
 
+
 import plotly.express as px
 import plotly.io as pio
 
 import time
+import itertools
+import joblib
+
 
 def mean_cellscore_fraction(cell_scores, ntop_factors=1):
     """ Get number of cells with a percentage of their total scores
@@ -260,7 +266,6 @@ def minibatch_ix_generator(ncells, batchsize):
         yield res
 
         
-from sklearn import metrics
 
 def get_param_dfs(model):
     eta_shp = pd.Series(np.ravel(model.eta.vi_shape), name=model.name)
@@ -364,10 +369,10 @@ def make_consensus_plot(factor_dists, cluster_labels, local_density, density_thr
     return fig, (dist_ax, left_ax, top_ax, hist_ax)
 
 
-def refit_local_params(X, global_params, nfactors, bp, dp, a=0.3, c=0.3, project_kw={}):
+# fit should be parallelized
+def refit_local_params(X, global_params, nfactors, bp, dp, a=0.3, c=0.3, single_process = False, project_kw={}):
     """
     """
-    start_time = time.time()
     project_defaults = dict(verbose=True, max_iter=50, check_freq=5)
     eta_shp, eta_rte, beta_shp, beta_rte = global_params
     
@@ -380,20 +385,349 @@ def refit_local_params(X, global_params, nfactors, bp, dp, a=0.3, c=0.3, project
     for k,v in project_defaults.items():
         if k not in project_kw.keys():
             project_kw[k] = v
-    loss = model.project(X, replace=True, **project_kw)
+    loss = model.project(X, replace=True, single_process=single_process, **project_kw)
     model.loss = loss
-    duration = (time.time() - start_time) / 60
-    print(f"refit_local_params: duration {duration} minutes.")
+    
     return model
 
 
-def get_ranked_genes(model, genes, name_col=1):
+def get_ranked_genes(model, genes):
     gene_score = model.gene_score()
 
     # rank the genes by gene_score
     ranks = np.argsort(gene_score, axis=0)[::-1]
     ranked_genes = []
     for i in range(gene_score.shape[1]):
-        ranked_genes.append(genes[ranks[:,i], name_col])
+        ranked_genes.append(genes[ranks[:,i]])
     ranked_genes = pd.DataFrame(np.stack(ranked_genes).T)
     return ranked_genes
+
+
+
+def model_selection(minK=0,maxK=0,nmodels=10,n_per=3,
+                    sample_folder=None, model_files=None,supercons=False):
+    if supercons and model_files is not None:
+        my_models = [joblib.load(m) for m in model_files]
+        nfactors = [m.nfactors for m in my_models]
+        minK=min(nfactors)
+        maxK=max(nfactors)
+        for m,n in zip(my_models,model_files):
+            m.name = n
+        return my_models, minK, maxK
+        
+    if not supercons and sample_folder is not None:
+        if min(range(minK,maxK+1)) < 10:
+            K_pattern = ['[0-9]','[0-9][0-9]']
+        if min(range(minK,maxK+1)) >= 10:
+            K_pattern = ['[0-9][0-9]']
+        if nmodels>0 and nmodels <=10:
+            n_pattern = ['[0-9]']
+        if nmodels <=100 and nmodels >10:
+            n_pattern = ['[0-9]','[0-9][0-9]']
+        model_files = [glob.glob(sample_folder+'/K'+k+'_n_'+n+'/scHPF*.joblib') \
+                       for k in K_pattern \
+                       for n in n_pattern]
+
+        model_files = [[m for m in model_files[s]
+                       if int(m.split('/K')[-1].split('_')[0]) >= minK
+                        and int(m.split('/K')[-1].split('_')[0]) <= maxK] 
+                       for s in range(len(model_files))] 
+
+
+        model_files=list(itertools.chain(*model_files))
+        model_files=sorted(model_files)
+        Ks = [int(m.split('scHPF_K')[1].split('_')[0]) for m in model_files]
+        ns = [int(m.split(sample_folder)[1].split('/')[1].split('_')[-1]) for m in model_files]
+
+        models = [joblib.load(m) for m in model_files]
+        models = [m[0] for m in models if type(m) is list]
+        nK=len(range(minK,maxK+1))
+        losses = np.nan * np.ones(shape=(nK,nmodels))
+        model_files_mtx = np.array([sample_folder for x in range(nK*nmodels)]).reshape((nK,nmodels))
+        model_files_mtx = model_files_mtx.astype('<U900')
+
+        for (K, n) in zip(Ks, ns):
+            search=sample_folder+'/K'+str(K)+'_n_'+str(n)+'/scHPF*.joblib'
+            model_template_name = glob.glob(search)
+            if len(model_template_name)>0:
+                model_template_name = model_template_name[0]
+                if model_template_name in model_files:
+                    model_files_mtx[K-minK][n] = model_template_name
+                    m = joblib.load(model_template_name)
+                    m = m[0] if type(m) is list else m
+                    losses[K-minK][n] = m.loss[-1]
+            rejected_models = []
+            for i in range(model_files_mtx.shape[0]):
+                temp = losses[i,:].copy()
+                id = sum(np.argwhere(~np.isnan(temp)).tolist(),[])
+                kept = pd.DataFrame(list(sorted(zip(losses[i,id], model_files_mtx[i,id]), reverse=True)[:n_per]))
+                kept = np.asarray(kept[:][1]) if kept.shape[0]>0 else None
+                for m in model_files_mtx[i,~np.isnan(temp)]:
+                    if m not in kept:
+                        rejected_models.append(m) 
+            rejected_models = sorted(rejected_models)
+
+        names = [m.split('/')[-1].rsplit('.',-1)[0] if m not in rejected_models 
+                 else m.split('/')[-1].rsplit('.',-1)[0]+'reject'
+                 for m in model_files]
+        for m,n in zip(models, names):
+            m.name = n
+
+        my_models = [m for m in models
+                     if ('reject' not in m.name)
+                        and m.nfactors >= minK
+                        and m.nfactors <= maxK] 
+
+        minK = np.min([m.nfactors for m in my_models])
+        maxK = np.max([m.nfactors for m in my_models])
+        print('n_models:', len(my_models),'minK:', minK, 'maxK:', maxK, 'n_per:', n_per)
+        return my_models
+
+
+
+def clustering (my_models,
+                minK=0, 
+                maxK=0,
+                n_top_genes=1000,
+                matrixfile,
+                genes,
+                sample_folder, 
+                sim=False, 
+                weighting_type='jaccard2',
+                cluster_type='walktrapP2', 
+                min_community_size=2,
+                sample=None, 
+                input_sample_list = None, 
+                steps=None, 
+                density_threshold = 2,
+                supercons = False):
+    
+    eta_shp, eta_rte, beta_shp, beta_rte = get_spectra(my_models)
+    eta_e_x = eta_shp/eta_rte
+    beta_e_x = beta_shp/beta_rte
+
+    spectra = get_genescore_spectra(my_models)
+    gene_ixs = (spectra.std()/spectra.mean()).astype(float).nlargest(n_top_genes).index.values # ordered genes by decreasing coefficients of variation
+    
+    outdir=sample_folder+'/consensus/'
+    outdir = outdir.replace('//','/')
+    if not os.path.exists(outdir):
+        os.mkdir(outdir) 
+
+    with PdfPages(outdir+'QC_spectra_mean_var.pdf',) as pdf:
+    # mean var relationship across spectra
+        jp = sns.jointplot(spectra.mean(), spectra.var(), kind='hex')
+        jp.set_axis_labels('mean', 'variance', fontsize=16)
+        pdf.savefig(bbox_inches = 'tight')
+    plt.close('all')
+
+    with PdfPages(outdir+'QC_spectra_mean_std.pdf') as pdf:
+        # mean std relationship across spectra
+        jp = sns.jointplot(spectra.mean(), spectra.std(), kind='hex')
+        jp.set_axis_labels('mean', 'std', fontsize=16)
+        pdf.savefig(bbox_inches = 'tight')
+    plt.close('all')
+    
+    with PdfPages(outdir+'QC_spectra_mean_var_top_genes.pdf') as pdf:
+        # where are selected genes from
+        # (check that we're getting some low mean genes but sticking to low-density areas)
+        plt.scatter(spectra.mean(), spectra.var(), c='tab:gray', s=5)
+        plt.scatter(spectra.mean()[gene_ixs], spectra.var()[gene_ixs], s=5)
+        plt.xlabel('mean') 
+        plt.ylabel('variance')
+        plt.semilogx()
+        pdf.savefig(bbox_inches = 'tight')
+    plt.close('all')
+
+    with PdfPages(outdir+'QC_spectra_mean_std_top_genes.pdf') as pdf:
+        plt.scatter(spectra.mean(), spectra.std(), c='tab:gray', s=5)
+        plt.scatter(spectra.mean()[gene_ixs], spectra.std()[gene_ixs], s=5)
+        plt.xlabel('mean')
+        plt.ylabel('std')
+        plt.semilogx()
+        pdf.savefig(bbox_inches = 'tight')
+    plt.close('all')   
+
+
+    transformed_spectra = spectra[gene_ixs]
+    factor_dists = 1 - pd.DataFrame(transformed_spectra, index=transformed_spectra.index).T.corr().values
+
+    local_neighborhood_size = 0.25  # just a heuristic for selecting k; 0.25
+    n_neighbors = max(5, int(local_neighborhood_size * len(my_models)))
+    local_density = get_local_density(factor_dists, n_neighbors, transformed_spectra.index)
+    print(f'{n_neighbors}/{transformed_spectra.shape[0]}')
+
+
+    with PdfPages(outdir+'KNN_'+str(n_neighbors)+'.Factor_Distance.pdf') as pdf:
+
+        plt.hist(local_density.values, bins=np.linspace(0,2,100))
+        plt.xlim(0, 1)
+        plt.xlabel('Median distance to k-nearest neighbors')
+        sns.despine()
+        pdf.savefig(bbox_inches = 'tight')
+    plt.close('all')
+
+    adj_binary = sklearn.neighbors.kneighbors_graph(factor_dists, n_neighbors, 
+                                                    metric='precomputed')
+    if sim:
+        adj_binary = adj_binary.multiply(adj_binary.T) # adjacency matrix with 2-length paths  (A^2)
+
+    if weighting_type in ['jaccard','jaccard2']:
+        adj = np.zeros(adj_binary.shape)
+        for i,j in np.stack(adj_binary.nonzero()).T:
+            adj[i,j] = metrics.jaccard_score(adj_binary[i,:].A[0], adj_binary[j,:].A[0])
+            if weighting_type=='jaccard2':
+                adj[i,j] = max(adj[i,j], 1e-5)
+        adj = sp.coo_matrix(adj)
+    elif weighting_type == 'clipCorr':
+        # (must convert dists to non-neg connectivities)
+        adj = sp.coo_matrix(adj_binary.A * np.clip(1-factor_dists,0,None)) #
+    elif weighting_type == 'shiftCorr':
+        adj = sp.coo_matrix(adj_binary.A * (2-factor_dists))
+    else:
+        assert False
+
+
+    np.random.seed(0)
+    
+    vcount = max(adj.shape)
+    sources, targets = adj.nonzero()
+    edgelist = list(zip(sources.tolist(), targets.tolist()))
+
+
+    if steps is None: 
+        steps = 4
+        
+    if density_threshold is None: 
+        density_threshold = 2
+
+
+    if cluster_type is None:
+        cluster_type = 'walktrapP2'
+
+    if cluster_type.startswith('walktrap'):
+        knn = ig.Graph(edges=edgelist, directed=False)
+        knn.vs['label'] = transformed_spectra.index
+        knn.es['width'] = adj.data
+        knn.es['weight'] = adj.data
+        cluster_result = knn.community_walktrap(weights=adj.data, steps=STEPS)
+
+        if cluster_type == 'walktrapP1':
+            nclusters = cluster_result.optimal_count + 1
+        elif cluster_type == 'walktrapP2':
+            nclusters = cluster_result.optimal_count + 2
+        else:
+            nclusters = cluster_result.optimal_count
+    else:
+        assert False
+    cluster_labels = pd.Series(cluster_result.as_clustering(nclusters).membership, index=transformed_spectra.index)
+    print(f'Nclusters: {len(cluster_labels.unique())} ({cluster_result.optimal_count})')
+
+    optimal_count = cluster_result.optimal_count 
+
+    x = np.arange(-(optimal_count-5),optimal_count+20)
+    modularity = [cluster_result.as_clustering(optimal_count + i).modularity for i in x]
+
+    plt.plot(x + optimal_count, modularity)
+    plt.axvline(nclusters, c='r')
+    plt.ylabel('modularity')
+    plt.xlabel('number of clusters')
+
+    outfile = f'{cluster_type}/supercons_min{minK}max{maxK}_n{n_per}' \
+        + f'.cv{n_top_genes}_k{n_neighbors}_{"sim_" if sim else ""}{weighting_type}_{cluster_type}' \
+        + f'{f"_s{steps}" if steps!=4 else ""}' \
+        + f'.modularity_peak.pdf'
+    print(outfile)
+    plt.savefig(outfile, bbox_inches='tight', transparent=True)
+
+    eta_shp_med = eta_shp.median().values # vector of length total ngenes
+    eta_rte_med = eta_rte.median().values
+    beta_shp_med = beta_shp.groupby(cluster_labels).median() # n of walktrap clusters * total ngenes
+    beta_rte_med = beta_rte.groupby(cluster_labels).median()
+
+    eta_e_x_med = eta_shp_med/eta_rte_med
+    beta_e_x_med = beta_shp_med/beta_rte_med
+
+    outfile = outfile.replace('modularity_peak', 'clustergram')
+    res = make_consensus_plot(factor_dists, cluster_labels, local_density, density_threshold=density_threshold)
+    res[0].savefig(outfile, bbox_inches='tight', transparent=True)
+
+
+
+    # removing small walktrap clusters
+    min_cluster_size = max(2,min_community_size) #int(local_neighborhood_size * len(my_models))
+    print("Walktrap community size cutoff = "+str(min_cluster_size))
+    print("The smallest community size = "+str(min(cluster_labels.value_counts())))
+    cl_keep = np.where(cluster_labels.value_counts(sort=False) >= min_cluster_size)[0]
+    print("# of communities kept = "+str(len(cl_keep)))
+    cluster_labels = cluster_labels.loc[cluster_labels.isin(cl_keep)]
+
+    X = mmread(source=matrixfile)
+    if supercons: 
+        print('super consensus across samples')
+        dense = X.tocsr()[:,:].todense()
+        X = coo_matrix(dense, shape=dense.shape, dtype=np.float64)
+    else:
+        print('consensus on sample: '+str(sample))
+        selected=np.where(np.isin(input_sample_list, sample, invert=False))[0]
+        X_sub = X.copy()
+        dense = X_sub.tocsr()[selected,:].todense()
+        X_sub = coo_matrix(dense, shape=dense.shape, dtype=np.float64)
+        X = X_sub
+        del X_sub
+            
+    MAX_PROJ = 1 
+
+    outfile = f'{outdir}/supercons_min{minK}max{maxK}_n{n_per}' \
+        + f'.cv{n_top_genes}_k{n_neighbors}_{"sim_" if sim else ""}{weighting_type}_{cluster_type}' \
+        + f'{f"_s{steps}" if steps!=4 else ""}' \
+        + f'.mcs{min_cluster_size}.proj_M{MAX_PROJ}.joblib'
+
+    if os.path.exists(outfile):
+        print(f'Loading from {outfile}')
+        model = joblib.load(outfile)
+    else:
+        np.random.seed(0)
+        nfactors = cluster_labels.nunique()
+        a = 0.3; c=0.3
+        for m in my_models:
+            if m.nfactors == nfactors:
+                a = m.a
+                c = m.c
+                break
+            
+        model = refit_local_params(X, (eta_shp_med, eta_rte_med, 
+                                       beta_shp_med.iloc[cl_keep], 
+                                       beta_rte_med.iloc[cl_keep]), 
+                               nfactors, 
+                               bp=my_models[0].bp, dp=my_models[0].dp,
+                               a=a, c=c,
+                               project_kw={'max_iter':MAX_PROJ})
+        print(f'Saving to {outfile}')
+        joblib.dump(model, outfile)
+
+    
+    outfile2 = outfile.replace('.joblib', f'.refit_M{MAX_REFIT}.joblib')
+    if os.path.exists(outfile2):
+        print(f'Loading from {outfile2}')
+        model2 = joblib.load(outfile2)
+    else:
+        test_loss = schpf.loss.projection_loss_function(
+        schpf.loss.mean_negative_pois_llh, X, model.nfactors, 
+        proj_kwargs={'reinit':False, 'verbose':False})
+        
+        model2 = deepcopy(model)
+        np.random.seed(0)
+        model2.fit(X, loss_function=test_loss, reinit=False, verbose=True, max_iter=MAX_REFIT)
+        print(f'Saving to {outfile2}')
+        joblib.dump(model2, outfile2)
+        
+    genes = np.loadtxt(genes, delimiter='\t', dtype=str)
+    top50genes = get_ranked_genes(model2, genes).head(50)
+    outprefix = outfile2.replace('.joblib', '')
+
+    top50genes.to_csv('{}_top50genes.txt'.format(outprefix), sep='\t', header=None,
+            index=None)
+    
+    return outfile2
+
